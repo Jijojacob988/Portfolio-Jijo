96fbb69bf251bb1155b6360b4e9abe63f76e3f0b
Jijo Jacob <Jijojacob988@gmail.com>
2025-02-09 15:05:35 +0530

    Deployed latest build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.png', '**/*.PNG'], // Add support for .png and .PNG files
+  base: '/Portfolio-Jijo/',  // ✅ Ensure correct base for GitHub Pages
+  assetsInclude: ['**/*.png', '**/*.PNG'],  // ✅ Fix PNG Import Issue
 });
